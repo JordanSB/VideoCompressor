2635ae3dc0f56ea1c7513dd8d7a10325a431dac7
Jordan Carroll <48207413+JordanSB@users.noreply.github.com>
2019-03-20 18:26:17 +0000

    Update MediaController.java

--- compressor/src/main/java/com/securebroadcast/compressor/videocompression/MediaController.java
+++ compressor/src/main/java/com/securebroadcast/compressor/videocompression/MediaController.java
@@ -38,9 +38,9 @@ public class MediaController {
     private boolean videoConvertFirstWrite = true;
 
     //Default values
-    private final static int DEFAULT_VIDEO_WIDTH = 1920;
-    private final static int DEFAULT_VIDEO_HEIGHT = 1080;
-    private final static int DEFAULT_VIDEO_BITRATE = 3000000;
+    private final static int DEFAULT_VIDEO_WIDTH = 1080;
+    private final static int DEFAULT_VIDEO_HEIGHT = 1920;
+    private final static int DEFAULT_VIDEO_BITRATE = 1000000;
 
     public static MediaController getInstance() {
         MediaController localInstance = Instance;
