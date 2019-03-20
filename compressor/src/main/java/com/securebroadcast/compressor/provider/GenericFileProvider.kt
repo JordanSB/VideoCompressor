cea38b7489ef844c8b905237b1281d62ccadd3f7
Jordan Carroll <48207413+JordanSB@users.noreply.github.com>
2019-03-20 17:34:43 +0000

    Revert "Converting to kotlin"

--- compressor/src/main/java/com/securebroadcast/compressor/provider/GenericFileProvider.kt
+++
@@ -1,5 +0,0 @@
-package com.securebroadcast.compressor.provider
-
-import android.support.v4.content.FileProvider
-
-class GenericFileProvider : FileProvider()
--- compressor/src/main/java/com/securebroadcast/compressor/videocompression/Config.java
+++ compressor/src/main/java/com/securebroadcast/compressor/videocompression/Config.java
@@ -1,8 +1,13 @@
-package com.securebroadcast.compressor.videocompression
+package com.securebroadcast.compressor.videocompression;
 
-//Static class to define general configuration values of the application
+/*
+* By Jorge E. Hernandez (@lalongooo) 2015
+* */
 
-object Config {
+/*
+* Static class to define general configuration values of the application
+* */
+public class Config {
 
     /**
      * Application root directory. All media files wi'll be stored here.
@@ -17,5 +22,5 @@ object Config {
     /**
      * Application folder for video files
      */
-    val VIDEO_COMPRESSOR_TEMP_DIR = "/Temp/"
+    public static final String VIDEO_COMPRESSOR_TEMP_DIR = "/Temp/";
 }
